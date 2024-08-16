3be7d7058c8cc1bf1650469c6945383e7ee639b5
rojag99 <GRoja@1999>
2024-08-16 17:24:23 +0530

    Code First Approach

--- WebApplication/WebApplication/Models/AppDbContext.cs
+++ WebApplication/WebApplication/Models/AppDbContext.cs
@@ -2,13 +2,27 @@
 
 namespace WebApplication.Models
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            
+
         }
         public DbSet<Employeedata> Employeedata { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employeedata>()
+                .ToTable("EmployeeData") 
+                .HasKey(e => e.Id);
+            modelBuilder.Entity<Employeedata>()
+        .Property(e => e.Name)
+        .HasMaxLength(100); 
 
+            modelBuilder.Entity<Employeedata>()
+                .Property(e => e.email)
+                .HasMaxLength(255);
+
+        }
     }
+
 }
